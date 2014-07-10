f8a128fcd222e883ee8b08cd6d06307644639d8e
Franklin Webber <franklin.webber@gmail.com>
2014-07-09 23:59:57 -0700

    New settings based on my new favorite org

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -5,9 +5,9 @@ log_level                :info
 log_location             STDOUT
 node_name                "franklinwebber"
 client_key               "#{current_dir}/franklinwebber.pem"
-validation_client_name   "frank-chef-aar-validator"
-validation_key           "#{current_dir}/frank-chef-aar-validator.pem"
-chef_server_url          "https://api.opscode.com/organizations/frank-chef-aar"
+validation_client_name   "franklinwebber-validator"
+validation_key           "#{current_dir}/franklinwebber-validator.pem"
+chef_server_url          "https://api.opscode.com/organizations/franklinwebber"
 cache_type               'BasicFile'
 cache_options( :path => "#{ENV['HOME']}/.chef/checksums" )
 cookbook_path            ["#{current_dir}/../cookbooks"]
