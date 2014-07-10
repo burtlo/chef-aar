847125291e30b2c14e74ccad8d4543d31b9be087
Franklin Webber <franklin.webber@gmail.com>
2014-07-09 23:52:41 -0700

    Allows Multiple Executions (Not Idempotent)
    So this addresses the biggest issue with the original recipe. The first recipe could only be executed once because there were no guards on marking when the database scripts had been run. In this implementation we use notifications to send run actions to our three scripts if and only if a database_prepared file is not present.

--- cookbooks/aar/recipes/default.rb
+++ cookbooks/aar/recipes/default.rb
@@ -128,20 +128,35 @@ end
 
 
 #
-# Executing the script file was an easy way to get the mysql instructions
-# installed. This really only works if root does not have a password which is
-# what is present by default. The remaining lines are not clear their operation
-# and actually the first two operations of running the script and creating the
-# user break the second time that this recipe is run make this a showstopper.
-#
-# ISSUE: Breaks on multiple execution, Ugly
+# This is a powerful concept that I did not understand when I first started
+# working with Chef. All the previous examples showed me sending notifications
+# to services. It is powerful being able to domino events like this with
+# notifications.
 #
 
-database_file = "/tmp/Awesome-Appliance-Repair-master/make_AARdb.sql"
+execute "Prepare Database" do
+  command "mysql < /tmp/Awesome-Appliance-Repair-master/make_AARdb.sql"
+  action :nothing
+end
 
-execute "mysql < #{database_file}"
-execute "mysql -e \"CREATE USER 'aarapp'@'localhost' IDENTIFIED BY '#{app_database_password}'\""
-execute "mysql -e \"GRANT CREATE,INSERT,DELETE,UPDATE,SELECT on AARdb.* to 'aarapp'@'localhost'\""
+execute "Create Database User" do
+  command "mysql -e \"CREATE USER 'aarapp'@'localhost' IDENTIFIED BY '#{app_database_password}'\""
+  action :nothing
+end
+
+execute "Grant User Privileges" do
+  command "mysql -e \"GRANT CREATE,INSERT,DELETE,UPDATE,SELECT on AARdb.* to 'aarapp'@'localhost'\""
+  action :nothing
+end
+
+file "Database Script Run Complete" do
+  path "/var/www/AAR/database_prepared"
+  action :create_if_missing
+  notifies :run, "execute[Prepare Database]"
+  notifies :run, "execute[Create Database User]"
+  notifies :run, "execute[Grant User Privileges]"
+end
+
 
 #
 # ISSUE: Not Idempotent
